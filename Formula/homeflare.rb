4b5cb12bea18594f276627a9e8cebf3f2e9f5cda
Timothy Schneider <tim@taslabs.net>
2026-03-30 12:37:40 -0400

    chore: add Apache 2.0 LICENSE, bump formula to v0.28.0

--- Formula/homeflare.rb
+++ Formula/homeflare.rb
@@ -1,8 +1,8 @@
 class Homeflare < Formula
   desc "The HomeFlare Project CLI - encrypted secret vault and more"
   homepage "https://github.com/HomeFlare"
-  url "https://registry.npmjs.org/@homeflare/hfs/-/hfs-0.26.0.tgz"
-  sha256 "6e60b3fd30f8c3ea428b42c7bcb8bff156190b24658adf1d4c668edb79c1603b"
+  url "https://registry.npmjs.org/@homeflare/hfs/-/hfs-0.28.0.tgz"
+  sha256 "676000fdec7cfa9159cbf36378992fc08899e37c36a7603854eef3aa55ec8bde"
   license "Apache-2.0"
 
   depends_on "node@22"
